e1263c3c48e985036ddada3c9852d8f50f729073
pavithra21635 <pavithra21635@gmail.com>
2024-08-18 19:56:22 +0530

    Installing necessary packages for authentication

---
+++ app.js
@@ -0,0 +1,5 @@
+const authentication = require('./authentication')
+const dataGenerator = require('./DataGenerator');
+
+
+authentication.startAuthenticate();
---
+++ authentication.js
@@ -0,0 +1,6 @@
+//packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const http = require('http');
+const dataGenerator = require('./DataGenerator');
