a71f0f9a75b4323a735688d7cd00af0602342808
pavithra21635 <pavithra21635@gmail.com>
2024-08-18 22:08:35 +0530

    Getting auth token and initializing data generation function

--- DataGenerator.js
+++ DataGenerator.js
@@ -3,3 +3,22 @@ const mongoose = require('mongoose');
 const faker = require('faker');
 const authentication = require('./authentication')
 require('dotenv').config();
+
+let authorizationToken;
+
+//getting aut token
+async function getSuccessToken(token){
+    authorizationToken = token;
+    await DataGeneration();
+
+}
+
+//starting data generation
+async function DataGeneration(authorizationToken){
+    await mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    });
+}
+
+module.exports = {getSuccessToken};
