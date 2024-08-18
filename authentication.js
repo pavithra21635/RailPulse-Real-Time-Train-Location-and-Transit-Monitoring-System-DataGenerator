3e629406cbe3747b86dd2db844ad9821aa7783a3
pavithra21635 <pavithra21635@gmail.com>
2024-08-18 20:17:18 +0530

    Initializing app and ports

--- authentication.js
+++ authentication.js
@@ -4,3 +4,11 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const http = require('http');
 const dataGenerator = require('./DataGenerator');
+
+//initialize app
+const app = express();
+//Port
+const PORT = process.env.AUTH_SERVER_PORT || 8080;
+const HOST = process.env.AUTH_SERVER_HOST || 'localhost';
+
+app.use(bodyParser.json());
