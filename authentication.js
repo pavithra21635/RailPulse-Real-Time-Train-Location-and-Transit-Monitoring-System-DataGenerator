e34356c557e9cecb4e0386990310c46ae8af7704
pavithra21635 <pavithra21635@gmail.com>
2024-08-18 21:59:12 +0530

    Authenticating with username and password

--- authentication.js
+++ authentication.js
@@ -12,3 +12,43 @@ const PORT = process.env.AUTH_SERVER_PORT || 8080;
 const HOST = process.env.AUTH_SERVER_HOST || 'localhost';
 
 app.use(bodyParser.json());
+
+//defining objects for username,password and key
+const databaseUsername = process.env.USERNAME;
+const databasePassword = process.env.PASSWORD;
+const key = 'SecretKey';
+
+//authenticating user
+function authenticateUser(username,password) {
+    return new Promise((resolve,reject) => {
+        if (username == databaseUsername && password == databasePassword){
+            const token = jwt.sign({ username: username }, key);
+            resolve(token);
+        } else {
+            reject(new Error('Invalid username or password'));
+        }
+    });
+}
+
+//starting authentication
+function startAuthenticate(){
+    app.listen(PORT, () => {
+        console.log(`Authentication server is running on port ${PORT}`);
+    });
+
+    authenticateUser(databaseUsername, databasePassword)
+    .then(token => {
+        console.log('Authentication successful');
+        dataGenerator.getSuccessToken(token);
+    })
+    .catch(error => {
+        console.error('Authentication failed:', error.message);
+ 
+    });
+
+}
+
+
+
+// module.exports = {startAuthenticate, user};
+module.exports = {startAuthenticate};
