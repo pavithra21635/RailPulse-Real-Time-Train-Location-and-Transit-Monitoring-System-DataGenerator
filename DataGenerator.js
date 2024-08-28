e566b6310eda5ae4e776910b0dbf7a2ad240a46f
pavithra21635 <pavithra21635@gmail.com>
2024-08-28 15:52:41 +0530

    Updating colombo to negombo correct cordinates and fixing the issue after cordinates finished movement

--- DataGenerator.js
+++ DataGenerator.js
@@ -33,22 +33,22 @@ async function DataGeneration(authorizationToken){
 
     // Define all trains and routes
     const trains = [
-        { trainId: 'Colombo to kandy', routeKey: 'route1' },
+       // { trainId: 'Colombo to kandy', routeKey: 'route1' },
         { trainId: 'Colombo to negombo', routeKey: 'route2' },
-        { trainId: 'Colombo to awissawella', routeKey: 'route3' },
-        { trainId: 'Colombo to puttalam', routeKey: 'route4' },
-        { trainId: 'Colombo to Galle', routeKey: 'route5' },
-        { trainId: 'Colombo to Polgahawela', routeKey: 'route6' },
-        { trainId: 'Colombo to Padukka', routeKey: 'route7' },
-        { trainId: 'Colombo to Mahawa', routeKey: 'route8' },
-        { trainId: 'Colombo to Anuradhapura', routeKey: 'route9' },
-        { trainId: 'Colombo to kurunegala', routeKey: 'route10' },
+       // { trainId: 'Colombo to awissawella', routeKey: 'route3' },
+       // { trainId: 'Colombo to puttalam', routeKey: 'route4' },
+        // { trainId: 'Colombo to Galle', routeKey: 'route5' },
+        // { trainId: 'Colombo to Polgahawela', routeKey: 'route6' },
+        // { trainId: 'Colombo to Padukka', routeKey: 'route7' },
+        // { trainId: 'Colombo to Mahawa', routeKey: 'route8' },
+        // { trainId: 'Colombo to Anuradhapura', routeKey: 'route9' },
+        // { trainId: 'Colombo to kurunegala', routeKey: 'route10' },
     ];
     
     // setInterval(generateAndSaveData, 30000);
     setInterval(() => {
         trains.forEach(train => generateAndSaveData(train.trainId, train.routeKey));
-    }, 500);
+    }, 1000);
     
 }
 
@@ -69,41 +69,41 @@ const TrainLocation = mongoose.model('TrainLocation', trainLocationSchema);
 
 // Initialize current waypoint index
 let currentWaypointIndices = {
-    'Colombo to kandy': 0,
+   // 'Colombo to kandy': 0,
     'Colombo to negombo': 0,
-    'Colombo to awissawella': 0, 
-    'Colombo to puttalam': 0, 
-    'Colombo to Galle': 0, 
-    'Colombo to Polgahawela': 0, 
-    'Colombo to Padukka': 0,
-    'Colombo to Mahawa': 0,
-    'Colombo to Anuradhapura': 0,
-    'Colombo to kurunegala': 0
+    // 'Colombo to awissawella': 0, 
+    // 'Colombo to puttalam': 0, 
+    // 'Colombo to Galle': 0, 
+    // 'Colombo to Polgahawela': 0, 
+    // 'Colombo to Padukka': 0,
+    // 'Colombo to Mahawa': 0,
+    // 'Colombo to Anuradhapura': 0,
+    // 'Colombo to kurunegala': 0
 };
 
-let travelDirections = {
-    'Colombo to Kandy': 'forward',
-    'Colombo to negombo': 'forward',
-    'Colombo to awissawella': 'forward', 
-    'Colombo to puttalam': 'forward', 
-    'Colombo to Galle': 'forward', 
-    'Colombo to Polgahawela': 'forward', 
-    'Colombo to Padukka': 'forward',
-    'Colombo to Mahawa': 'forward',
-    'Colombo to Anuradhapura': 'forward',
-    'Colombo to kurunegala': 'forward'
-};
+// let travelDirections = {
+//    // 'Colombo to Kandy': 'forward',
+//     'Colombo to negombo': 'forward',
+//     // 'Colombo to awissawella': 'forward', 
+//     // 'Colombo to puttalam': 'forward', 
+//     // 'Colombo to Galle': 'forward', 
+//     // 'Colombo to Polgahawela': 'forward', 
+//     // 'Colombo to Padukka': 'forward',
+//     // 'Colombo to Mahawa': 'forward',
+//     // 'Colombo to Anuradhapura': 'forward',
+//     // 'Colombo to kurunegala': 'forward'
+// };
 
 
 // Function to generate and send train location data
 async function generateAndSaveData(trainId, routeKey) {
     let currentWaypointIndex = currentWaypointIndices[trainId];
-    const route = trainRoute[routeKey];
-    const totalWaypoints = route.length;
-    const waypoint = route[currentWaypointIndex];
+    // const route = trainRoute[routeKey];
+    // const totalWaypoints = route.length;
+    // const waypoint = route[currentWaypointIndex];
 
 
-    //const waypoint = trainRoute[routeKey][currentWaypointIndex];
+    const waypoint = trainRoute[routeKey][currentWaypointIndex];
        
         const locationData = {
             trainId: trainId,
@@ -124,23 +124,23 @@ async function generateAndSaveData(trainId, routeKey) {
         console.error('Error saving location data to database:', err);
     }
 
-    // Move to the next waypoint
-    //currentWaypointIndices[trainId] = (currentWaypointIndices[trainId] + 1) % trainRoute[routeKey].length;
+    //Move to the next waypoint
+    currentWaypointIndices[trainId] = (currentWaypointIndices[trainId] + 1) % trainRoute[routeKey].length;
     
     // Update the waypoint index based on direction
-    if (travelDirections[trainId] === 'forward') {
-        currentWaypointIndex = (currentWaypointIndex + 1) % totalWaypoints;
-        if (currentWaypointIndex === 0) {
-            travelDirections[trainId] = 'backward';
-        }
-    } else {
-        currentWaypointIndex = (currentWaypointIndex - 1 + totalWaypoints) % totalWaypoints;
-        if (currentWaypointIndex === totalWaypoints - 1) {
-            travelDirections[trainId] = 'forward';
-        }
-    }
+    // if (travelDirections[trainId] === 'forward') {
+    //     currentWaypointIndex = (currentWaypointIndex + 1) % totalWaypoints;
+    //     if (currentWaypointIndex === 0) {
+    //         travelDirections[trainId] = 'backward';
+    //     }
+    // } else {
+    //     currentWaypointIndex = (currentWaypointIndex - 1 + totalWaypoints) % totalWaypoints;
+    //     if (currentWaypointIndex === totalWaypoints - 1) {
+    //         travelDirections[trainId] = 'forward';
+    //     }
+    // }
 
-    currentWaypointIndices[trainId] = currentWaypointIndex;
+    // currentWaypointIndices[trainId] = currentWaypointIndex;
 }
 
 
